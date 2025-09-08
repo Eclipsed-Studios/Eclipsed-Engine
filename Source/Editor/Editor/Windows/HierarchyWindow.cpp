3e644eace9fadd5abd85b77febaf9a7acc1d9738
ZultooX <zultoox@gmail.com>
2025-09-08 07:00:04 +0200

    Update HierarchyWindow.cpp

--- Source/Editor/Editor/Windows/HierarchyWindow.cpp
+++ Source/Editor/Editor/Windows/HierarchyWindow.cpp
@@ -2,12 +2,32 @@
 
 #include "ImGui/imgui.h"
 #include "ECS/ComponentManager.h"
+#include "Components/Transform2D.h"
 
+#include "Input/Input.h"
 
 namespace ENGINE_NAMESPACE::Editor
 {
 	void HierarchyWindow::Update()
 	{
+        if (ImGui::BeginPopupContextWindow("##CTX_MENU_RIGHT_CLICK", ImGuiPopupFlags_MouseButtonRight))
+        {
+			if (ImGui::BeginMenu("Create new..."))
+			{
+				if (ImGui::MenuItem("GameObject")) 
+				{ 
+					// TODO: Fix a gameobject creation manager
+					static int id = 1000;
+					ComponentManager::AddComponent<Transform2D>(id++);
+				}
+
+				ImGui::EndMenu();
+			}
+
+            ImGui::EndPopup();
+        }
+
+
 		for (const auto& [id, data] : ComponentManager::myEntityIdToEntityData)
 		{
 			if (ImGui::Button(std::string(data.name + "##" + std::to_string(id)).c_str()))
