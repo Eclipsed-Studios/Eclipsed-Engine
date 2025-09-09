f84f1afb47104dcdccf46caba6bfc01108484f95
ZultooX <zultoox@gmail.com>
2025-09-09 08:36:45 +0200

    d

--- Source/Editor/Editor/ComponentRegistry.h
+++ Source/Editor/Editor/ComponentRegistry.h
@@ -11,14 +11,32 @@ namespace ENGINE_NAMESPACE::Editor
 {
 	class ComponentRegistry 
 	{
-	public:
-		static void RegisterComponent(const std::string& name, std::function<void(int)> addComponentMethod);
+    public:
+        static void RegisterComponent(const std::string& name, std::function<void(int)> addComponentMethod) 
+        {
+            GetMap()[name] = addComponentMethod;
+        }
 
-		static void AddComponent(const std::string& name, int id);
+        static void AddComponent(const std::string& name, int id) 
+        {
+            auto& map = GetMap();
+            auto it = map.find(name);
+            if (it != map.end()) 
+            {
+                it->second(id);
+            }
+        }
 
-		static const std::unordered_map<std::string, std::function<void(int)>>& GetComponents();
+        static const std::unordered_map<std::string, std::function<void(int)>>& GetComponents() 
+        {
+            return GetMap();
+        }
 
-	private:
-		static inline std::unordered_map<std::string, std::function<void(int)>> components;
+    private:
+        static std::unordered_map<std::string, std::function<void(int)>>& GetMap() 
+        {
+            static std::unordered_map<std::string, std::function<void(int)>> map;
+            return map;
+        }
 	};
 }
