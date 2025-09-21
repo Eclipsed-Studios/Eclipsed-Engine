4c2d74e2b7663f247c4f2699aa2f432d17a6645a
ZultooX <zultoox@gmail.com>
2025-09-21 14:26:20 +0200

    Update Reflection_Macros.h

--- Eclipse-Source/Source/Utilities/Utilities/Reflection/Reflection_Macros.h
+++ Eclipse-Source/Source/Utilities/Utilities/Reflection/Reflection_Macros.h
@@ -5,14 +5,24 @@
 #include "Reflection.h"
 #include "SerializedVariable.h"
 
+// Will draw a custom inspector for this component.
 #define SERIALIZED_FIELD_DEFAULT(Type, Name, DefaultValue) Eclipse::Reflection::SerializedVariable<Type> Name{#Name, this, true, DefaultValue}
+
+// Will draw a custom inspector for this component.
 #define SERIALIZED_FIELD(Type, Name) Eclipse::Reflection::SerializedVariable<Type> Name{#Name, this, true}
 
 
+// Will draw custom inspector for this component with min and max values.
 #define SERIALIZED_FIELD_DEFAULT_MIN_MAX(Type, Name, DefaultValue, Min, Max) Eclipse::Reflection::SerializedVariable<Type> Name{#Name, this, true, DefaultValue, Min, Max}
+
+// Will draw custom inspector for this component with min and max values.
 #define SERIALIZED_FIELD_MIN_MAX(Type, Name, Min, Max) Eclipse::Reflection::SerializedVariable<Type> Name{#Name, this, true, Min, Max}
 
+
+// Will not draw an inspector and just be serializable.
 #define PRIVATE_SERIALIZED_FIELD_DEFAULT(Type, Name, DefaultValue) Eclipse::Reflection::SerializedVariable<Type> Name{#Name, this, false, DefaultValue}
+
+// Will not draw an inspector and just be serializable.
 #define PRIVATE_SERIALIZED_FIELD(Type, Name) Eclipse::Reflection::SerializedVariable<Type> Name{#Name, this, false}
 
 
