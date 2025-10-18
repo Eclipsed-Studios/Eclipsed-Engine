44edb37b9deedbd0a40a2e2342014387badaa3d8
Christopher Vedlund <zultoox@gmail.com>
2025-10-18 02:26:32 +0200

    Fixed engine includes so they include themselves

--- Tools/generate-project.bat
+++ Tools/generate-project.bat
@@ -1,6 +1,6 @@
 @echo off
 
-cmake -G "Visual Studio 17 2022" -T host=x64 -S ../Eclipse-Source/ -B ../build -DEDITOR=OFF
+cmake -G "Visual Studio 17 2022" -T host=x64 -S ../Eclipse-Source/ -B ../Eclipse-Source/build -DEDITOR=ON
       
       
 
