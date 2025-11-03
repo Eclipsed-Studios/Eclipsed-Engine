85b06444fb20b2d960c590e2f5990fcbf1a1b0a1
Christopher Vedlund <zultoox@gmail.com>
2025-11-03 13:39:16 +0100

    Add File utility class and integrate with CookedAssetWriter
    Introduces a new File utility class for file operations in Utilities/Files, providing methods for reading, writing, and creating files. CookedAssetWriter is updated to use this new File class for writing cooked assets, improving file handling consistency.

--- Eclipse-Source/Source/Engines/AssetEngine/AssetEngine/CookedAssetWriter.cpp
+++ Eclipse-Source/Source/Engines/AssetEngine/AssetEngine/CookedAssetWriter.cpp
@@ -3,11 +3,17 @@
 #include <fstream>
 #include "AssetEngine/PathManager.h"
 
+#include "Utilities/Files/File.h"
+
 namespace Eclipse::Assets
 {
 	void CookedAssetWriter::Write(const CookedAsset& cookedAsset)
 	{
-		std::string path = (PathManager::GetCookedAssetsDir() / (std::to_string(cookedAsset.id) + ".asset")).generic_string();
+		const std::filesystem::path path = (PathManager::GetCookedAssetsDir() / (std::to_string(cookedAsset.id) + ".asset"));
+		const std::string pathString = path.generic_string();
+
+		Utilities::File file(pathString.c_str(), std::ios::binary | std::ios::out);
+
 		std::ofstream out(path, std::ios::binary);
 		out.write(cookedAsset.data.data(), cookedAsset.size);
 		out.close();
---
+++ Eclipse-Source/Source/Utilities/Utilities/Files/File.cpp
@@ -0,0 +1,67 @@
+#include "File.h"
+
+#include <filesystem>
+#include <fstream>
+
+namespace Eclipse::Utilities
+{
+	File::File()
+	{
+	}
+
+
+	File::File(const char* path, int ios)
+	{
+		std::filesystem::path fullPath = path;
+
+		std::string fileName = fullPath.filename().string();
+		std::string dirPath = fullPath.parent_path().generic_string();
+
+		File::Create(dirPath.c_str(), fileName.c_str());
+
+		stream = std::fstream(path, ios);
+		if (stream.is_open()) streamOpened = true;
+	}
+
+	Eclipse::Utilities::File::~File()
+	{
+		stream.close();
+		streamOpened = false;
+	}
+
+	void File::Create(const char* dirPath, const char* fileName)
+	{
+		std::filesystem::create_directories(dirPath);
+		std::ofstream file(fileName);
+		file.close();
+	}
+
+	void File::Write(const char* str, int size, int idx)
+	{
+		if (str == nullptr) return;
+
+		stream.seekp(idx);
+		stream.write(str, size);
+	}
+
+	void File::WriteLine(const char* str, int size)
+	{
+		stream.seekp(writeLineIdx);
+		stream.write(str, size);
+		stream.write("\n", 1);
+		writeLineIdx = stream.tellp();
+	}
+
+	void File::Read(char* outStr, int size, int idx)
+	{
+		if (outStr == nullptr) return;
+
+		stream.seekg(idx);
+		stream.read(outStr, size);
+	}
+
+	std::string File::ReadLine()
+	{
+		return "";
+	}
+}
