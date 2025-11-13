cdb5f58b5fcc97e6caa121f457c4a7e828e1fd24
Christopher Vedlund <zultoox@gmail.com>
2025-11-13 19:20:58 +0100

    Added engine files

--- Eclipse-Source/Source/Engine/EclipsedEngine/EclipsedEngine/EclipsedEngine.cpp
+++ Eclipse-Source/Source/Engine/EclipsedEngine/EclipsedEngine/EclipsedEngine.cpp
@@ -1,1 +1,25 @@
+#include "EclipsedEngine.h"
 
+namespace Eclipse
+{
+	void EclipsedEngine::Update()
+	{
+		Internal_BeginFrame();
+		Internal_Update();
+		Internal_EndFrame();
+	}
+
+
+	void EclipsedEngine::Internal_Update()
+	{
+
+	}
+
+	void EclipsedEngine::Internal_EndFrame()
+	{
+	}
+
+	void EclipsedEngine::Internal_BeginFrame()
+	{
+	}
+}
--- Eclipse-Source/Source/Engine/EclipsedEngine/EclipsedEngine/EclipsedEngine.h
+++ Eclipse-Source/Source/Engine/EclipsedEngine/EclipsedEngine/EclipsedEngine.h
@@ -1,1 +1,18 @@
+#pragma once
 
+namespace Eclipse
+{
+	class EclipsedEngine
+	{
+	public:
+		void StartEngine();
+		
+		void Update();
+		
+	private:
+		void Internal_BeginFrame();
+		void Internal_Update();
+		void Internal_EndFrame();
+
+	};
+}
