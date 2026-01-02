baafd5ef42dc1970cbf1ad6f84acacb40c6580c1
Christopher Vedlund <zultoox@gmail.com>
2026-01-02 20:23:02 +0100

    Refactor asset structure and update build outputs
    Moved EngineAssets and DLLs into the Assets directory and updated all references accordingly. Added new DLLs (SDL3, Discord SDK, FMOD) to Assets/DLLs. Updated CMake files to use EXE_OUTPUT_DIR/Libs for output directories. Modified PathManager to use current_path as engine source root. Improved generate-project.bat to copy assets and headers to the correct locations.

--- Tools/generate-project.bat
+++ Tools/generate-project.bat
@@ -1,8 +1,17 @@
 @echo off
 
 cmake -G "Visual Studio 17 2022" -T host=x64 -S ../Eclipse-Source/ -B ../Eclipse-Source/Library/Binary -DECLIPSED_EDITOR=ON
-      
-      
+    
+cd ..
+cd Eclipse-Source
+
+xcopy "Assets/DLLs\*" "Bin" /E /I /Y
+xcopy "Assets/EngineAssets" "Bin/EngineAssets" /E /I /Y
+
+set SOURCE=Source
+set DEST=Bin/Headers
+
+robocopy "%SOURCE%" "%DEST%" *.h *.inl *.hpp /S /E /R:0 /W:0
 
 
 pause
