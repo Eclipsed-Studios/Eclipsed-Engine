002efb145262e7f8ba312efa27e206d8cddfc57d
Christopher Vedlund <zultoox@gmail.com>
2026-02-08 19:05:29 +0100

    Templatize LoadOrCreateMeta and add meta type
    Convert LoadOrCreateMeta to a templated, header-only function (defaulting to AssetMetaSettings) and remove the old non-template implementation from the .cpp. Update the importer call to use the templated form. Add a new AssetMetaSettings-derived struct (SimonsR�vh�l) with an `age` field and serialization to support a different meta type.

--- Eclipse-Source/Source/Engine/Modules/AssetEngine/AssetEngine/Editor/Importer/ImportSettings/AssetImportSettings.h
+++ Eclipse-Source/Source/Engine/Modules/AssetEngine/AssetEngine/Editor/Importer/ImportSettings/AssetImportSettings.h
@@ -17,4 +17,20 @@ namespace Eclipse
 			);
 		}
 	};
+
+	struct SimonsRövhål : public AssetMetaSettings
+	{
+		int age;
+
+		template <class Archive>
+		void serialize(Archive& ar)
+		{
+			AssetMetaSettings::serialize(ar);
+
+			ar(
+				CEREAL_NVP(age)
+			)
+		}
+	};
+
 }
