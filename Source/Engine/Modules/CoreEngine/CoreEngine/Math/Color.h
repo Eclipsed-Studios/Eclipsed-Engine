0d3ace003bbd6e6a38a08cddd0280d355f464a07
8nooThing8 <93975705+8nooThing8@users.noreply.github.com>
2026-05-13 19:19:08 +0200

    moving implementation of color and give variables in vector better names

--- Source/Engine/Modules/CoreEngine/CoreEngine/Math/Color.h
+++ Source/Engine/Modules/CoreEngine/CoreEngine/Math/Color.h
@@ -5,34 +5,19 @@
 
 namespace Eclipse::Math
 {
+	constexpr float twoFify = 1.f / 255.f;
+
 	class Color final
 	{
 	public:
 		Color() = default;
 		
-		Color(float R, float G, float B, float A)
-		: r(R), g(G), b(B), a(A)
-		{
+		Color(float R, float G, float B, float A);
+		Color(int aHexValue);
 
-		}
-		Color(int aHexValue)
-		{
-			r = ((aHexValue >> 16) & 0xFF) / 255.f;
-			g = ((aHexValue >> 8) & 0xFF) / 255.f;
-			b = ((aHexValue) & 0xFF) / 255.f;
-		}
+		int32_t GetHexValue();
 
-		int32_t GetHexValue() 
-		{
-			int R = static_cast<int>(r * 255.f);
-			int G = static_cast<int>(g * 255.f);
-			int B = static_cast<int>(b * 255.f);
-			int A = static_cast<int>(a * 255.f);
-
-			return (R << 24) | (G << 16) | (B << 8) | A;
-		}
-
-		Vector4f ToVector() { return { r, g, b, a }; }
+		Vector4f ToVector();
 
 		SERIALIZE(MAKE_NVP(r),
 			MAKE_NVP(g),
