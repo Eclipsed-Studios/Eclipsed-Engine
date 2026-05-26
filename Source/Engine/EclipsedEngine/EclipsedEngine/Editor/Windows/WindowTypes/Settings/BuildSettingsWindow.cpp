3b347def1ee28571235af2feaed6874b5e27c881
Christopher Vedlund <zultoox@gmail.com>
2026-05-26 16:56:10 +0200

    Fixed text rendering in game build

---
+++ Source/Engine/EclipsedEngine/EclipsedEngine/Editor/Windows/WindowTypes/Settings/BuildSettingsWindow.cpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#ifdef ECLIPSED_EDITOR
+
+#include "EclipsedEngine/Editor/Windows/WindowTypes/Settings/BuildSettingsWindow.h"
+#include "EclipsedEngine/Editor/Game/GameCompiler.h"
+#include "CoreEngine/PathManager.h"
+#include "AssetEngine/AssetManager.h"
+
+namespace Eclipse::Editor
+{
+	void BuildSettingsWindow::Update()
+	{
+		ImGui::Text("Debug");
+		ImGui::SameLine();
+		ImGui::Checkbox("##is_debug_build", &isDebugBuild);
+
+		if (ImGui::Button("Build"))
+			Build();
+	}
+
+	void BuildSettingsWindow::Build()
+	{
+		std::filesystem::remove_all(PathManager::GetProjectRoot() / "Build");
+		std::filesystem::create_directory(PathManager::GetProjectRoot() / "Build");
+
+		Assets::AssetManager::PackAssets();
+
+		BatchScript script(
+			PathManager::GetEngineRoot().parent_path() / "Tools",
+			"build-game-editor.bat",
+			{
+				"-DPROJECT_PATH=" + PathManager::GetProjectRoot().generic_string(),
+				"-DENGINE_PATH=" + PathManager::GetEngineRoot().parent_path().generic_string(),
+				"-DCONFIG=" + std::string(isDebugBuild ? "Debug" : "Release")
+			}
+		);
+
+		script.Run(true);
+	}
+}
+
+#endif
