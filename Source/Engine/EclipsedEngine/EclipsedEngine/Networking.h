b76751b5dbb1c6d353dd1f92e5ea947c1219b9d6
8nooThing8 <93975705+8nooThing8@users.noreply.github.com>
2026-06-11 19:37:29 +0200

    Engine shutdown on Engine or Game shutdown

--- Source/Engine/EclipsedEngine/EclipsedEngine/Networking.h
+++ Source/Engine/EclipsedEngine/EclipsedEngine/Networking.h
@@ -2,16 +2,7 @@
 
 #ifdef ECLIPSED_NETWORKING
 	#define SHUT_DOWN_NETWORK_ENGINE() \
-		if (MainSingleton::Exists<SteamP2PNetworkingServer>()) \
-		{ \
-			auto& server = MainSingleton::GetInstance<SteamP2PNetworkingServer>(); \
-			/*server.ShutDown();*/ \
-		} \
-		if (MainSingleton::Exists<SteamP2PNetworkingClient>()) \
-		{ \
-			auto& client = MainSingleton::GetInstance<SteamP2PNetworkingClient>(); \
-			/*client.ShutDown();*/ \
-		}
+		Replication::ReplicationManager::CloseConnection("Disconnect : Quit Game");
 #else
 	#define SHUT_DOWN_NETWORK_ENGINE() 
 #endif
