6f73c3a519341e44ad8948a300c2d26445b561e3
Christopher Vedlund <zultoox@gmail.com>
2026-08-24 23:49:46 +0200

    Fixed parallax objects so it looks nicer. Also implemented y scaling

--- Source/Game/Game/ParallaxBackground.h
+++ Source/Game/Game/ParallaxBackground.h
@@ -2,26 +2,6 @@
 
 #include "EclipsedEngine/Components/Component.h"
 
-namespace Eclipse
-{
-	class SpriteRenderer2D;
-}
-
-class ParallaxBackgroundObject : public Eclipse::Component
-{
-	COMPONENT_BASE_2(ParallaxBackgroundObject, 0);
-
-public:
-	void Start() override;
-	void Update() override;
-
-private:
-	float startingPosX = 0.f;
-	float posY = 0.f;
-	float rot = 0.f;
-	Eclipse::SpriteRenderer2D* rend;
-};
-
 class ParallaxBackground : public Eclipse::Component
 {
 	COMPONENT_BASE_2(ParallaxBackground, 0);
